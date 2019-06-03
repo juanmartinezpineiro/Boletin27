13b6313d151a5d124e4bc288574825253ac0dfaf
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2019-06-03 11:03:11 +0200

    Boletin terminado

---
+++ src/Boletin27.java
@@ -0,0 +1,12 @@
+package boletin27;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class Boletin27 {
+    public static void main(String[] args) {
+        Calculadora obxCalc=new Calculadora();
+        obxCalc.setVisible(true);
+    }
+}
